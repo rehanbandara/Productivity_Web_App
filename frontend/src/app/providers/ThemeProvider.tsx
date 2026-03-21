d8f8141bc767a25d80dd619fa9335e385df3fc24
Wishwaka67 <wishwakabasnayaka16667@gmail.com>
2026-03-21 19:06:22 +0530

    Remove frontend folder

--- frontend/src/app/providers/ThemeProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
-import theme from "@/theme/muiTheme";
-import { ReactNode } from "react";
-
-export default function ThemeProvider({ children }: { children: ReactNode }) {
-    return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
-}
